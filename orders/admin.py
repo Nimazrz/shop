d621269634127108aad636520dc8bf886b2d7f3a
Nimazrz <Nimaaa8413@gmail.com>
2024-11-13 00:36:09 +0330

    export to excel file

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
 from .models import *
+import openpyxl
+from django.http import HttpResponse
+
+
 # Register your models here.
+
+def export_to_excel(modeladmin, request, queryset):
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=orders.xlsx'
+    # --------------------
+    wb = openpyxl.Workbook()
+    ws = wb.active
+    ws.title = "Orders"
+    # --------------------
+    columns = ['ID', 'First Name', 'Last Name', 'Phone', 'Address', 'Postal Code',
+               'Province', 'City', 'Paid', 'Created']
+    ws.append(columns)
+    # --------------------
+    for order in queryset:
+        created = order.created.replace(tzinfo=None) if order.created else ''
+        ws.append([
+            order.id, order.first_name, order.last_name, order.phone, order.address,
+            order.postal_code, order.province, order.city, order.paid, created
+        ])
+    wb.save(response)
+    return response
+
+
+export_to_excel.short_description = 'Export to Excel'
+
 
 
 # class AddressInline(admin.StackedInline):
@@ -15,6 +44,12 @@ class OrderItemInline(admin.TabularInline):
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ['id','buyer', 'first_name', 'last_name', 'phone', 'paid', 'created', 'updated']
+    list_display = ['id', 'buyer', 'first_name', 'last_name', 'phone', 'paid', 'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
-    inlines = [OrderItemInline,]  # AddressInline
+    inlines = [OrderItemInline, ]  # AddressInline
+    actions = [export_to_excel]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['product']
